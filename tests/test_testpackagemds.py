393bf9989675ecf3f0f9595f8655b82f1004a542
florencia <florencia.dandrea@gmail.com>
2022-01-23 21:17:30 -0800

    Modifications

--- tests/test_testpackagemds.py
+++ tests/test_testpackagemds.py
@@ -1,6 +1,4 @@
 from testpackagemds.testpackagemds import count_words
-from testpackagemds.plotting import plot_words
-from testpackagemds.datasets import get_flatland
 from collections import Counter
 import matplotlib
 import pytest
@@ -20,42 +18,3 @@ def test_count_words(einstein_counts):
     assert actual == expected, "Einstein quote counted incorrectly!"
 
 
-def test_plot_words(einstein_counts):
-    """Test plotting of word counts."""
-    fig = plot_words(einstein_counts)
-    assert isinstance(fig, matplotlib.container.BarContainer), \
-           "Wrong plot type"
-    assert len(fig.datavalues) == 10, \
-           "Incorrect number of bars plotted"
-
-@pytest.mark.parametrize(
-    "obj",
-    [
-        3.141,
-        "test.txt",
-        ["list", "of", "words"]
-    ]
-)
-def test_plot_words_error(obj):
-    """Check TypeError raised when Counter not used."""
-    with pytest.raises(TypeError):
-        list_object = ["Pythons", "are", "non", "venomous"]
-        plot_words(obj)
-
-
-def test_integration():
-    """Test count_words() and plot_words() workflow."""
-    counts = count_words("tests/einstein.txt")
-    fig = plot_words(counts)
-    assert isinstance(fig, matplotlib.container.BarContainer), \
-           "Wrong plot type"
-    assert len(fig.datavalues) == 10, \
-           "Incorrect number of bars plotted"
-    assert max(fig.datavalues) == 2, "Highest word count should be 2"
-
-
-def test_regression():
-    """Regression test for Flatland"""
-    top_word = count_words(get_flatland()).most_common(1)
-    assert top_word[0][0] == "the", "Most common word is not 'the'"
-    assert top_word[0][1] == 2245, "'the' count has changed"
